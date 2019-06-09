5ba8f14e0ab4d0f6e4910d6da5c42e1649bcd941
therealchiendat <chiendat@gmail.com>
2019-06-09 15:39:52 -0300

    Add Search + Styling

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,46 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper,Polyline} from 'google-maps-react';
-import api from "./Resources/sensitive/api.json";
-import trip0 from "./Resources/trips/2016-07-02--11-56-24.json"; // <-- Sample data import
-import trip1 from "./Resources/trips/2016-07-02--13-09-31.json";
-import "./Home.css";
-let i;
+import { makeStyles } from '@material-ui/core/styles';
+import api from './Resources/sensitive/api.json';
+import {Paper, InputBase, IconButton, List, ListItem, ListItemText, ListItemLink} from '@material-ui/core/';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import './Home.css';
+
+const searchstyle = makeStyles({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 'auto',
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+});
+
+
+const SearchComponent = () =>{
+  const classes = searchstyle();
+  return (
+    <Paper className={classes.root}>
+      <InputBase
+        className={classes.input}
+        placeholder='Search for events'
+        inputProps={{ 'aria-label': 'Search for events' }}
+      />
+      <IconButton className={classes.iconButton} aria-label='Search'>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  )
+}
+
+
 export class Home extends React.Component {
 
   constructor(props) {
@@ -16,46 +52,55 @@ export class Home extends React.Component {
 		};
 	}
 
-  heatMapColorforValue(value){
-    var h = (40 - value)/40 * 150
-    return "hsl(" + h + ", 100%, 50%)";
-  }
-  renderpolyline(coords,speed){
-    var indents = [];
-    for(var i = 0; i < coords.length-1; i++){
-      indents.push(
-        <Polyline
-          path={[coords[i],coords[i+1]]}
-          strokeColor={speed[i]}
-          strokeOpacity={1}
-          strokeWeight={3}
-          />
-        );
-        };
-    return indents;
-  }
-  render() {
-    const coords=[{lat:trip0.coords[0].lat,lng:trip0.coords[0].lng}];
-    const speed=[this.heatMapColorforValue(trip0.coords[0].speed)];
-    for (i=1; i<trip0.coords.length; i++) {
-      coords.push({lat:trip0.coords[i].lat,lng:trip0.coords[i].lng});
-      speed.push(this.heatMapColorforValue(trip0.coords[i].speed));
-    }
-    console.log(speed[1])
-    const style = {
-      width: 'auto',
-      height: '80vh',
-      position: 'relative'
-    }
 
+  render() {
+    const mapstyle = {
+      width: 'auto',
+      height: '75vh',
+      position: 'relative',
+    }
     return (
-      <div container style={style}>
-      <Map google={this.props.google}
-          onClick={this.onMapClicked}
-          zoom={10}
-          initialCenter= {{lat:45.9299084,lng:-66.6647803}}
-          >
-      </Map>
+      <div container  className='Home'>
+        <div className='Searchsection'
+          style={{
+            'background-color':'white',
+            'margin-bottom':'20px',
+            'padding':'20px',
+            'border-radius':'10px',
+            'box-shadow': '0 4px 4px 0 rgba(0,0,0,0.1)'}}>
+          <SearchComponent />
+          <List>
+            <ListItem button>
+              <ListItemText primary='Fredericton Rib Fest' />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary='LGBT Pride Parade' />
+            </ListItem>
+
+            <ListItem button>
+              <ListItemText primary='Wild Karaoke' />
+            </ListItem>
+          </List>
+        </div>
+        <div
+        style={{
+          'background-color':'white',
+          'width': 'auto',
+          'position': 'relative',
+          'height': '80vh',
+          'padding-top':'20px',
+          'border-radius':'10px',
+          'box-shadow': '0 4px 4px 0 rgba(0,0,0,0.1)'}}
+        >
+        <Map
+            style={mapstyle}
+            google={this.props.google}
+            onClick={this.onMapClicked}
+            zoom={10}
+            initialCenter= {{lat:45.9299084,lng:-66.6647803}}
+            >
+        </Map>
+        </div>
       </div>
     )
   }
